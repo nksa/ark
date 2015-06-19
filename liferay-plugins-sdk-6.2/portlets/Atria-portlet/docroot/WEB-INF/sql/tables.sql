eacd80497f81d396f1d09436f1e907f8e3e8b600
nikolaos.xanthopoulos <nikolaos.xanthopoulos@eurodyn.com>
2015-06-19 17:56:31 +0300

    SQL changes to be committed:

--- liferay-plugins-sdk-6.2/portlets/Atria-portlet/docroot/WEB-INF/sql/tables.sql
+++ liferay-plugins-sdk-6.2/portlets/Atria-portlet/docroot/WEB-INF/sql/tables.sql
@@ -14,20 +14,3 @@ create table vega_Contact (
 	relative BOOLEAN,
 	image BLOB
 );
-
-create table vega_Foo (
-	personId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	firstName VARCHAR(75) null,
-	lastName VARCHAR(75) null,
-	nickName VARCHAR(75) null,
-	birthday DATE null,
-	age INTEGER,
-	friend BOOLEAN,
-	relative BOOLEAN,
-	image BLOB
-);
